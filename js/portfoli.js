5efb38f2f938a285cba1b6ad950fd7e275061d8e
jide mobell <jidd32@gmail.com>
2017-05-22 09:18:43 -0400

    portfolio commit

---
+++ js/portfoli.js
@@ -0,0 +1,27 @@
+$(document).ready(
+    function() {
+        $(".fa-top").hover(function() {
+            $(this).addClass("bounce");
+
+        }, function() {
+            $(this).removeClass("bounce");
+        });
+
+
+        $(".imgborder").hover(function() {
+            $(this).addClass("shake");
+
+        }, function() {
+            $(this).removeClass("shake");
+        });
+
+
+
+
+
+
+
+
+
+    }
+);
